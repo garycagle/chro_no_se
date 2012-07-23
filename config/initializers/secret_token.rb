decf90f5b80d3817c1630c3ef4c585eebcfa0300
Gary Cagle <gcagle@bitstream.net>
2012-07-23 14:20:29 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChroNoSe::Application.config.secret_token = '9615c8cc1d9cabde4bfd64efae182f77fdb7576c38d75bf0ba3b68db21945a766505e937ea386c8aa2947ecab079089d6fd60f5facbbe40e433906fa550a4623'
